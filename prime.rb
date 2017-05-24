315748ecdc10fc35238caa385051dbbf98b8d105
Chaitanya Krishna <ckchaitanyasunny@gmail.com>
2017-05-24 05:57:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  if number <= 2
+    return false
+  else
+    (2..number-1).to_a.each { |e| return false if number%e==0 }
+  end
+  return true
+end
+
+prime?(4)
